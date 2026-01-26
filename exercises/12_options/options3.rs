7096154a7c4d44ec7b0c0bfbe908a1df44d5fdab
phil.markgraf <pmarkgraf@tandemdiabetes.com>
2026-01-26 12:19:23 -0800

    Work from 2026-01-26
    Co-authored-by: Bassam Esa <bassamesa-dev@users.noreply.github.com>
    Co-authored-by: Aldo Suarez Diaz <asuarezdiaz@users.noreply.github.com>
    Co-authored-by: Jake Eshelman <jake11212@users.noreply.github.com>
    Co-authored-by: Matthew Gold <mgold@tandemdiabetes.com>

--- exercises/12_options/options3.rs
+++ exercises/12_options/options3.rs
@@ -6,10 +6,12 @@ struct Point {
 
 fn main() {
     let optional_point = Some(Point { x: 100, y: 200 });
+    // let mut x : Point;
 
     // TODO: Fix the compiler error by adding something to this match statement.
     match optional_point {
-        Some(p) => println!("Co-ordinates are {},{}", p.x, p.y),
+        Some(ref p) => println!("Co-ordinates are {},{}", p.x, p.y),
+        // Some(p) => {x = Some(optional_point).unwrap()},
         _ => panic!("No match!"),
     }
 
